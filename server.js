d5ecf92ca52077bb898371403c2ee9218eaf2d01
unknown <amsubhadeep@gmail.com>
2023-03-09 22:29:53 +0530

    Merged socket and apollo-server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers/index");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({
-        headers: req.headers
-    })
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({
+    headers: req.headers,
+  }),
 });
 
 // Connect to DB
@@ -18,25 +18,25 @@ const PORT = process.env.PORT || 3001;
 const URL = process.env.DATABASE_URL;
 
 mongoose
-    .connect(URL)
-    .then(() => {
-        console.log("Successfully connected to database");
-        return server.listen({ port: PORT });
-    })
-    .then((res) => {
-        console.log(`Server running at ${res.url}`);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+  .connect(URL)
+  .then(() => {
+    console.log("Successfully connected to database");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-// Socket
-/*
-const { Server, Socket } = require("socket.io");
-const io = new Server(9000, {
-    cors: {
-        origin: "*"
-    }
+/*** SocketIO ***/
+
+const { Server } = require("socket.io");
+const io = new Server(3002, {
+  cors: {
+    origin: "*",
+  },
 });
 
 const RECEIVE_CONVERSATION = "receive-conv";
@@ -45,23 +45,17 @@ const SEND_CONVERSATION = "send-conv";
 const SEND_MESSAGE = "send-message";
 
 io.on("connection", (clientSocket) => {
-    const id = clientSocket.handshake.query.id;
-    clientSocket.join(id);
-    console.log("Connected: " + clientSocket.id);
+  const id = clientSocket.handshake.query.id;
+  clientSocket.join(id);
+  console.log("Connected: " + clientSocket.id);
 
-    // data = { conversationId, recipient, senderId, senderName, message}
+  clientSocket.on(SEND_MESSAGE, (data) => {
+    clientSocket.emit(RECEIVE_MESSAGE, data);
+    clientSocket.to(data.recipient).emit(RECEIVE_MESSAGE, data);
+  });
 
-    clientSocket.on(SEND_MESSAGE, (data) => {
-        clientSocket.emit(RECEIVE_MESSAGE, data);
-        clientSocket.to(data.recipient).emit(RECEIVE_MESSAGE, data);
-        // console.log("Send message to user " + data.recipient + " from user " + clientSocket.id);
-        // console.log(data);
-    });
-
-    clientSocket.on(SEND_CONVERSATION, (data) => {
-        clientSocket.emit(RECEIVE_CONVERSATION, data);
-        clientSocket.to(data.recipient).emit(RECEIVE_CONVERSATION, data);
-        // console.log("Send conv to user " + data.recipient);
-    });
+  clientSocket.on(SEND_CONVERSATION, (data) => {
+    clientSocket.emit(RECEIVE_CONVERSATION, data);
+    clientSocket.to(data.recipient).emit(RECEIVE_CONVERSATION, data);
+  });
 });
-*/
