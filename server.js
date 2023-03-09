7d21656d5e92e1068038f038a4eebc7d4a24b90f
unknown <amsubhadeep@gmail.com>
2023-03-09 20:51:03 +0530

    Fixed project

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
 });
 
 // Connect to DB
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 const URL = process.env.DATABASE_URL;
 
 mongoose
